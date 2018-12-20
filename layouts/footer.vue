1fb02db916dda5647062e1c2e61ed42bb39db126
nozomi0966 <nozomotoitech@gmail.com>
2018-12-20 22:23:08 +0900

    add contents

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <div>GeekWebPageFooter</div>
+    <div>Footer</div>
   </footer>
 </template>
 
--- layouts/header.vue
+++ layouts/header.vue
@@ -1,6 +1,16 @@
 <template>
   <header>
-    <div>GeekWebPage</div>
+    <div class="header-topbar">
+      <div class-"header-topbar-home">
+        <nuxt-link to="/">ホーム</nuxt-link>
+      </div>
+      <div>
+        <a href="http://www.u-aizu.ac.jp/">会津大学公式サイト</a>
+      </div>
+      <div>
+        English
+      </div>
+    </div>
   </header>
 </template>
 
