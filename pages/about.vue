ff04d0409ad3ca09f9a58318abb590b62fa088e2
nozomi0966 <nozomotoitech@gmail.com>
2018-12-21 22:05:39 +0900

    Impl about page

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <div class="content">
+      About Page
+    </div>
+    <div class="photo">
+      <img src="../assets/images/geekdojo1.png">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" src="../style/page/about/style.scss" scoped>
+</style>
