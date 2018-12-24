8936784d94f5151fd8f058c711ba776897d8eb7f
nozomi0966 <nozomotoitech@gmail.com>
2018-12-24 12:00:57 +0900

    add link to banner

--- components/banner.vue
+++ components/banner.vue
@@ -2,16 +2,24 @@
   <div class="banner">
     <ul class="banner-ul">
       <li class="banner-li">
-        <img src="../assets/images/banner/bnr_osip.gif">
+        <a href="https://www.u-aizu.ac.jp/osip/">
+          <img src="../assets/images/banner/bnr_osip.gif">
+        </a>
       </li>
       <li class="banner-li">
-        <img src="../assets/images/banner/bnr_aizuac.gif">
+        <a href="https://www.u-aizu.ac.jp/sgu/img/banner/bnr_aizuac.gif">
+          <img src="../assets/images/banner/bnr_aizuac.gif">
+        </a>
       </li>
       <li class="banner-li">
-        <img src="../assets/images/banner/bnr_movie.gif">
+        <a href="https://www.u-aizu.ac.jp/sgu/img/banner/bnr_movie.gif">
+          <img src="../assets/images/banner/bnr_movie.gif">
+        </a>
       </li>
       <li class="banner-li">
-        <img src="../assets/images/banner/bnr_top.gif">
+        <a href="https://www.u-aizu.ac.jp/sgu/img/banner/bnr_top.gif">
+          <img src="../assets/images/banner/bnr_top.gif">
+        </a>
       </li>
     </ul>
   </div>
