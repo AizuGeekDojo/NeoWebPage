27d65f34a7f4748266148bad6fea0ab3f782bd66
nozomi0966 <nozomotoitech@gmail.com>
2018-12-26 12:55:48 +0900

    opentime indexページのレスポンシブ化

--- pages/opentime.vue
+++ pages/opentime.vue
@@ -13,13 +13,6 @@
             frameborder="0"
             scrolling="no"/>
         </div>
-        <div class="responsive-iframe-container small-container"> 
-          <iframe
-            src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=646mrbqs7kg1g7869hatdlan6o%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=Asia%2FTokyo"
-            style="border:solid 1px #777"
-            frameborder="0"
-            scrolling="no"/>>
-        </div>
       </div>
     </div>
   </div>
