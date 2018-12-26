64d321f57f11532442ced5437bcedfd05ac0564c
nozomi0966 <nozomotoitech@gmail.com>
2018-12-26 12:55:18 +0900

    footerのレスポンシブ化

---
+++ layouts/footetr_mini.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <div class="footer-bar">
+      <img src="../assets/uaizu.png">
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" src='../style/footer_mini/style.scss' scoped>
+</style>
