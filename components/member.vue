5b7048a251d698020ff245d76ad1726e2dad7f4e
nozomi0966 <nozomotoitech@gmail.com>
2019-07-03 14:25:46 +0900

    add members page and member component

---
+++ components/member.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="member">
+    <div class="picture">
+      <img :src="picture_path">
+    </div>
+    <div class="name">
+      <p> {{ name }}</p>
+    </div>
+    <div class="detail">
+      <p> {{ detail }}</p>
+    </div>
+    <div class="like">
+      <p> {{ like }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    picture_path: {
+      type: String,
+      required: true
+    },
+    detail: {
+      type: String,
+      required: true
+    },
+    like: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/color';
+.member {
+  background-color: $subColor3;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.36863);
+  height: 300px;
+  width: 600px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+
+  display: grid;
+  grid-template-rows: 60% 40%;
+  grid-template-columns: 40% 1fr;
+  .picture {
+    grid-row: 1/2;
+    grid-column: 1/2;
+    text-align: center;
+    margin: 10px;
+    img {
+      height: 100%;
+      background-size: cover;
+      border-radius: 50%;
+    }
+  }
+  .name {
+    grid-row: 2/3;
+    grid-column: 1/2;
+    text-align: center;
+  }
+  .detail {
+    grid-row: 1/2;
+    grid-column: 2/3;
+    text-align: center;
+  }
+  .like {
+    grid-row: 2/3;
+    grid-column: 2/3;
+    text-align: center;
+  }
+}
+</style>
---
+++ pages/members.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="top">
+    <div
+      class="members content">
+      <div
+        v-for="(member, index) in members"
+        :key="index"
+      >
+        <member
+          :name="member.name"
+          :picture_path="member.picture_path"
+          :detail="member.detail"
+          :like="member.like"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import member from '@/components/member'
+export default {
+  components: {
+    member: member
+  },
+  data: function() {
+    return {
+      members: [
+        {
+          name: 'nozo-moto',
+          picture_path:
+            'https://scontent-nrt1-1.cdninstagram.com/vp/77887d988fd60fda07bc6a2ccb505368/5DAA2F85/t51.2885-15/e35/16123024_1896395307256157_2583947978490249216_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com',
+          detail: 'ほげほげ',
+          like: 'golang, python'
+        },
+        {
+          name: 'y-namiki',
+          picture_path:
+            'https://scontent-nrt1-1.cdninstagram.com/vp/77887d988fd60fda07bc6a2ccb505368/5DAA2F85/t51.2885-15/e35/16123024_1896395307256157_2583947978490249216_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com',
+          detail: '最強',
+          like: 'golang, python'
+        },
+        {
+          name: 'hatobus',
+          picture_path:
+            'https://scontent-nrt1-1.cdninstagram.com/vp/77887d988fd60fda07bc6a2ccb505368/5DAA2F85/t51.2885-15/e35/16123024_1896395307256157_2583947978490249216_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com',
+          detail: 'Twitterのバズり芸人',
+          like: 'golang, python'
+        },
+        {
+          name: 'hoge fugato',
+          picture_path:
+            'https://scontent-nrt1-1.cdninstagram.com/vp/77887d988fd60fda07bc6a2ccb505368/5DAA2F85/t51.2885-15/e35/16123024_1896395307256157_2583947978490249216_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com',
+          detail: 'piyo piyo',
+          like: 'golang, python'
+        }
+      ],
+      breadcrumbs: [
+        {
+          name: 'メンバー',
+          path: '/members'
+        }
+      ]
+    }
+  },
+  created() {
+    this.$store.commit('change_page', this.breadcrumbs)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/color';
+.top {
+  /*TODO heightは適当に調整する*/
+  .members {
+    width: 80%;
+    margin: 0 auto;
+  }
+}
+</style>
