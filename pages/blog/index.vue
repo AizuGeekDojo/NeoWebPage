fc0ae85a4f6987e8711aba137eb85c0d516a3bd9
nozomi0966 <nozomotoitech@gmail.com>
2019-07-12 02:18:01 +0900

    add blog portal interface

---
+++ pages/blog/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="top">
+    <div class="content">
+      <div>
+        TODO
+        なんとか記事リストみたいなのを作る
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      breadcrumbs: [
+        {
+          name: 'blog',
+          path: '/blog'
+        }
+      ]
+    }
+  },
+  created: function() {
+    this.$store.commit('change_page', this.breadcrumbs)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.top {
+  width: 100%;
+  height: 100%;
+}
+</style>
