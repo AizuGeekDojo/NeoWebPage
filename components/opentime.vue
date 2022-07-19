2e527efb397edb43849a055a4cd48d81f77ebc1d
shinbunbun <takamuneyuto@gmail.com>
2022-07-19 15:39:09 +0900

    move opentime

---
+++ components/opentime.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="top">
+    <div class="content">
+      <h2>開室日程</h2>
+      <p>Aizu Geek Dojo は以下の時間にSAが開けています。<br>使用の予約等をしたい際には、<a href="https://forms.gle/2kQpNrg6MPkhDqkV8" target="_blank" rel="noopener noreferrer">予約フォーム</a>からしてください。</p>
+      <div class="opentime-calender">
+        <div class="gc_wrapper">
+          <div class="responsive-iframe-container big-container">
+            <iframe
+              src="https://calendar.google.com/calendar/embed?src=p39fcq3e5glaod0u4ukm9pol18%40group.calendar.google.com&ctz=Asia%2FTokyo"
+              style="border:solid 1px #777"
+              width="100%"
+              height="500px"
+              frameborder="0"
+              scrolling="no"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../style/color';
+.opentime-calender {
+  padding: 0;
+  position: relative;
+  width: 100%;
+  height: auto;
+}
+</style>
--- pages/opentime.vue
+++ pages/opentime.vue
@@ -1,28 +1,14 @@
 <template>
-  <div class="top">
-    <div class="content">
-      <h2>開室日程</h2>
-      <p>Aizu Geek Dojo は以下の時間にSAが開けています。<br>使用の予約等をしたい際には、<a href="https://docs.google.com/forms/u/3/d/e/1FAIpQLSe_7ULD_MkPpy7Y8iSpfF8R6_mIUzGgd-ANeaAhFxDHmrhIfA/viewform">予約フォーム</a>からしてください。</p>
-      <div class="opentime-calender">
-        <div class="gc_wrapper">
-          <div class="responsive-iframe-container big-container">
-            <iframe
-              src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=646mrbqs7kg1g7869hatdlan6o%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=Asia%2FTokyo"
-              style="border:solid 1px #777"
-              width="100%"
-              height="500px"
-              frameborder="0"
-              scrolling="no"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <opentime />
 </template>
 
 <script>
+import opentime from '@/components/opentime.vue'
+
 export default {
+  components: {
+    opentime
+  },
   data: function() {
     return {
       breadcrumbs: [
@@ -38,13 +24,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import '../style/color';
-.opentime-calender {
-  padding: 0;
-  position: relative;
-  width: 100%;
-  height: auto;
-}
-</style>
