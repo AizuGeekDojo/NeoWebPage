6bc6b65b6940826faa5b7f235d2e81a0e45ed4a4
shinbunbun <takamuneyuto@gmail.com>
2022-07-20 14:06:14 +0900

    fix link

--- pages/instraction.vue
+++ pages/instraction.vue
@@ -13,12 +13,12 @@
         </li>
         <li>
           <h3>講習会資料</h3>
-          <p><a href="https://github.com/AizuGeekDojo/LaserCutterInstruction/blob/master/doc_pdf/LaserCutter1.pdf">資料</a></p>
+          <p><a href="https://github.com/AizuGeekDojo/LaserCutterInstruction/blob/master/doc/LaserCutter1.md" target="_blank" rel="noopener noreferrer">資料</a></p>
         </li>
         <li>
           <h3>受講するには</h3>
           <p>予約フォームにアクセスしていただき手続きを進めてください。<br>予約日の2日前までに予約を完了させておいてください。</p>
-          <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSe_7ULD_MkPpy7Y8iSpfF8R6_mIUzGgd-ANeaAhFxDHmrhIfA/viewform">予約フォーム</a></p>
+          <p><a href="https://forms.gle/HqpDtpGse7oBtSGp8" target="_blank" rel="noopener noreferrer">予約フォーム（学生用）</a></p>
         </li>
       </ul>
     </div>
